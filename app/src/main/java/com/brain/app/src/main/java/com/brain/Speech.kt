78ec4cdd33f7f3b9b5c3eeacb035d3082ac63844
drntoday <drn.today@outlook.com>
2026-01-20 00:05:49 +0530

    Create Speech.kt

---
+++ app/src/main/java/com/brain/app/src/main/java/com/brain/Speech.kt
@@ -0,0 +1,12 @@
+package com.brain
+
+class Speech {
+
+    fun listen(): String {
+        return "user speech text"
+    }
+
+    fun speak(text: String) {
+        // TTS will come here
+    }
+}
