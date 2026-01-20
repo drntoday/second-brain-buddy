02407cbd421a54a886e3fa1273fb364237bfd2bb
drntoday <drn.today@outlook.com>
2026-01-20 08:30:24 +0530

    Update Voice.kt

--- app/src/main/java/com/brain/Voice.kt
+++ app/src/main/java/com/brain/Voice.kt
@@ -1,18 +1,33 @@
 package com.brain
 
 import android.content.Context
+import android.media.AudioManager
 import android.speech.tts.TextToSpeech
 import java.util.*
 
-class Voice(context: Context) {
+class Voice(val context: Context) {
 
     var tts: TextToSpeech
+    val audio =
+        context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
 
     init {
-        tts = TextToSpeech(context) {}
+        tts = TextToSpeech(context) {
+            // ready
+        }
     }
 
     fun speak(text: String, lang: String) {
+
+        // --- WHISPER SETTINGS ---
+        audio.setStreamVolume(
+            AudioManager.STREAM_MUSIC,
+            3,              // low volume for privacy
+            0
+        )
+
+        // prefer earphones
+        audio.mode = AudioManager.MODE_IN_COMMUNICATION
 
         val locale = when(lang){
             "hindi" -> Locale("hi","IN")
@@ -21,6 +36,12 @@ class Voice(context: Context) {
         }
 
         tts.language = locale
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
+
+        val params = HashMap<String, String>()
+        params[TextToSpeech.Engine.KEY_PARAM_VOLUME] = "0.4"
+
+        tts.speak(text,
+            TextToSpeech.QUEUE_FLUSH,
+            params)
     }
 }
